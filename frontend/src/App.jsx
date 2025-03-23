3e1cb92611a355c496d60c3206859bcb7f7da095
Divyansh singh bisen <thakurdivyanshbisen19@gmail.com>
2025-03-23 13:03:58 +0530

    again

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
     formData.append("formId", formId);
     
     try {
-      const response = await axios.post("autoquizz-production.up.railway.app/upload", formData);
+      const response = await axios.post("https://autoquizz-production.up.railway.app", formData);
       setQuestions(response.data.questions);
     } catch (error) {
       console.error("Upload failed:", error);
