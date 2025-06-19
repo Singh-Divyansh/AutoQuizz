bf28d7e3642ffcc3288508770b29045b2e2eddd8
Divyansh singh bisen <thakurdivyanshbisen19@gmail.com>
2025-06-20 01:01:16 +0530

    edit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -33,7 +33,7 @@ export default function App() {
     formData.append("formId", formId);
     
     try {
-      const response = await axios.post("http://localhost:5000/upload", formData);
+      const response = await axios.post("https://autoquizz.onrender.com/upload", formData);
       setQuestions(response.data.questions);
     } catch (error) {
       console.error("Upload failed:", error);
